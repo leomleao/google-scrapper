0b90e16556f05b9487cc8172357627253fd21f09
Leonardo Leao <leomleao@gmail.com>
2022-08-05 16:22:01 +0100

    Axios does't wait to load all the content, I try to implement with Puppeter isntead

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
+const bluebird = require('bluebird');
+const cliProgress = require('cli-progress');
 
 const params = {
     headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36"
     },
+    timeout: 2000
 };
 
-const customers = require('cakebase')("./test.json");
+const customers = require('cakebase')("./test2.json");
 
-async function Hello() {
+async function getCustomers() {
     let customerList = await customers.get(obj => obj.title === '');
-    customerList.forEach(customer => {
-        getTitle(customer).then((result) => {
-            users.update(obj => obj.customer === result.customer, { title: result.title, address: result.address });
-        })
-    });
+    b1.start(customerList.length, 0);
+    return customerList
 }
+let customerList = getCustomers();
 
-Hello();
-
-// let newFile = file.map(customer =>{
-//     return getTitle(customer).then(test => {
-//         // return test
+// customerList.forEach(customer => {
+//     getTitle(customer).then((result) => {
+//         customers.update(obj => obj.customer === result.customer, { title: result.title, address: result.address });
 //     })
-// })
+// });
 
-// Promise.all(newFile).then(completed => {
-//     fs.writeFile(fileName, JSON.stringify(completed, null, 2), function writeJSON(err) {
-//         if (err) return console.log(err);
-//     });
-// })
+const b1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-function getTitle(row) {
-    let encodedString = encodeURI(row.searchString);
-    return axios
-        .get(
-            `https://www.google.com/search?q=${encodedString}&hl=en&gl=us`,
-            params
-        ).then(function ({ data }) {
-            let $ = cheerio.load(data);
-            row.title = $(".SPZz6b > h2 > span").text()
-            row.address = $(".LrzXr").text()
-            return row
-        });
+async function makeRequests (lines) {
+    await bluebird.map(
+        lines,
+        async (line) => {
+            const encodedString = encodeURI(line.searchString);
+            console.info(`https://www.google.com/search?q=${encodedString}&hl=en&gl=us`)
+            return axios.get(
+                `https://www.google.com/search?q=${encodedString}&hl=en&gl=us`,
+                params
+            ).then(res => {
+                b1.increment();
+                const $ = cheerio.load(res.data);
+                line.title = $(".SPZz6b > h2 > span").text()
+                line.address = $(".LrzXr").text()
+                customers.update(obj => obj.customer === line.customer, { title: line.title, address: line.address });
+            })
+        },
+        { concurrency: 2 }
+    )
 }
+makeRequests(customerList)
+
+// function getTitle(row) {
+//     let encodedString = encodeURI(row.searchString);
+//     return axios
+//         .get(
+//             `https://www.google.com/search?q=${encodedString}&hl=en&gl=us`,
+//             params
+//         ).then(function ({ data }) {
+//             let $ = cheerio.load(data);
+//             row.title = $(".SPZz6b > h2 > span").text()
+//             row.address = $(".LrzXr").text()
+//             return row
+//         });
+// }
 
 
 
